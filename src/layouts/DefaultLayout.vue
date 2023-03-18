19a6297f91934a1089dc23034454983b062883bd
Suhong Kim <totoku103@gmail.com>
2023-03-18 21:52:18 +0900

    feat: add font-awesome-icon

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -3,14 +3,14 @@
     <div class="w-30 bg-white border-r border-gray-200 p-4">
       <div class="text-lg font-bold mb-4">Menu</div>
       <ul>
-        <li class="mb-2">
+        <li class="mb-2 flex">
           <router-link to="/" class="block py-2 px-4 hover:bg-gray-200 rounded">
-            HOME
+            <font-awesome-icon icon="fa-solid fa-house" /> HOME
           </router-link>
         </li>
         <li class="mb-2">
           <router-link to="/test" class="block py-2 px-4 hover:bg-gray-200 rounded">
-            TEST
+            <font-awesome-icon icon="fa-solid fa-vials" /> TEST
           </router-link>
         </li>
       </ul>
--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,23 @@ import Routes from './routes'
 // highcharts
 import * as Highcharts from 'highcharts'
 import Exporting from 'highcharts/modules/exporting'
-
 Exporting(Highcharts)
+
+//awesome
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {far} from '@fortawesome/free-regular-svg-icons'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab)
+
+
 
 const app = createApp(App)
 app.use(Routes)
 
 app.component('chart', Highcharts)
+    .component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
 
